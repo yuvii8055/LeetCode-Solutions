dd817bcadc75be2b1f3fd4d148fe101c76226507
Yuvaraj.L <1dt25ci122@dsatm.edu.in>
2026-09-22 13:09:00 +0000

    Sync LeetCode submission Runtime - 11 ms (52.14%), Memory - 19.4 MB (20.91%)

---
+++ 0006-zigzag-conversion/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+            
+        rows = [""] * numRows
+        current_row = 0
+        going_down = False 
+        
+        for char in s:
+            rows[current_row] += char
+            
+            if current_row == 0 or current_row == numRows - 1:
+                going_down = not going_down
+                
+            if going_down:
+                current_row += 1
+            else:
+                current_row -= 1
+                
+        return "".join(rows)
